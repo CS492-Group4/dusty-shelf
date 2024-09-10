5d6f7243602c1e091ad62133b9e129a0c87ddb34
Ayexil <leinadkun@gmail.com>
2024-09-09 20:50:16 -0400

    new file:   .idea/dusty-shelf.iml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	new file:   db.sqlite3 	new file:   dusty_shelf/__init__.py 	new file:   dusty_shelf/__pycache__/__init__.cpython-310.pyc 	new file:   dusty_shelf/__pycache__/settings.cpython-310.pyc 	new file:   dusty_shelf/__pycache__/urls.cpython-310.pyc 	new file:   dusty_shelf/__pycache__/wsgi.cpython-310.pyc 	new file:   dusty_shelf/asgi.py 	renamed:    web_project/web_project/settings.py -> dusty_shelf/settings.py 	new file:   dusty_shelf/urls.py 	new file:   dusty_shelf/wsgi.py 	new file:   manage.py 	renamed:    web_project/manage.py -> web_project/hello/manage.py 	modified:   web_project/hello/models.py 	new file:   web_project/hello/settings.py 	modified:   web_project/hello/views.py

--- web_project/hello/views.py
+++ web_project/hello/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from pymongo import MongoClient
 
-# Create your views here.
+# MongoDB Connection
+client = MongoClient("mongodb+srv://mongodbstudent1:t4aK6RZdC4QE3eM4@cluster0.6cclx.mongodb.net/")
+db = client["DustyShelf"]
+books_collection = db["books"]
+
+# Function to add a book
+def add_book(request):
+    name = request.GET.get('name')
+    author = request.GET.get('author')
+    price = float(request.GET.get('price'))
+    quantity = int(request.GET.get('quantity'))
+    
+    book = {
+        "title": name,
+        "author": author,
+        "price": price,
+        "quantity": quantity
+    }
+    result = books_collection.insert_one(book)
+    return HttpResponse(f"Book added with ID: {result.inserted_id}")
