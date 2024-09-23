a4b43c60156c1c4ad8c71e35b8a2265ca38c880d
CS492-Group4 <seth.fair1@student.ctuonline.edu>
2024-09-22 22:16:36 -0500

    Update settings.py

--- dusty_shelf/settings.py
+++ dusty_shelf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#1%gfxeluku@t+37l4fjv7@^2m(+75)pe^3&_#wv1^%e!yw$(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
