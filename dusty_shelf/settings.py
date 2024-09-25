729a5b050d52a19789bb44ffb628a3e8ee969c40
buteossys <buteossystems@gmail.com>
2024-09-25 12:22:38 -0500

    prepared a staticfiles file and updated settings to allow django to serve azure staic files

--- dusty_shelf/settings.py
+++ dusty_shelf/settings.py
@@ -137,7 +137,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware', 
+              'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
